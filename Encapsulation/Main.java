15cd1e770d9d60f8d6a063314140bd8363631fd9
Bruce Tushabe <brucetushabe@gmail.com>
2022-07-20 09:11:31 +0300

    Improved the code

--- Encapsulation/Main.java
+++ Encapsulation/Main.java
@@ -6,7 +6,13 @@ public class Main {
 
         employee e = new employee();
 
-        e.getName();
+        e.setName("Bruce");
+        e.setId(1888);
+        e.setSalary(120000);
+
+        System.out.println("Name: " + e.getName());
+        System.out.println("ID: " + e.getId());
+        System.out.println("Salary: " + e.getSalary());
 
     }
 }
