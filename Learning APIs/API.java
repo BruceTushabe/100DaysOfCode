71c63d177e22964f5fb9c169f6721032afc5a410
brucetushabe <brucetushabe@gmail.com>
2022-07-29 00:27:10 +0300

    *

--- Learning APIs/API.java
+++ Learning APIs/API.java
@@ -1,12 +1,12 @@
 import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.net.InetSocketAddress; 
+import java.net.InetSocketAddress;
 
 
 public class API {
 
-    public public static void main(String[] args) throws IOException{
+    public static void main (String [] args) throws IOException{
 
         HttpServer server = HttpServer.create( new InetSocketAddress(8080), 0);
         server.createContext("api/greeting", exchange -> {
