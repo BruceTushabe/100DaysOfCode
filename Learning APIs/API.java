28311ee0f09e0610421f63ad4778ce8ae7ebb8ca
brucetushabe <brucetushabe@gmail.com>
2022-07-31 07:54:30 +0300

    Just practicing

--- Learning APIs/API.java
+++ Learning APIs/API.java
@@ -9,7 +9,7 @@ public class API {
     public static void main (String [] args) throws IOException{
 
         HttpServer server = HttpServer.create( new InetSocketAddress(8080), 0);
-        server.createContext("api/greeting", exchange -> {
+        server.createContext("api/greeting", (exchange -> {
             if ("GET" .equals(exchange.getRequestMethod())) {
                 String responseText = "Hello World! from our framework-less REST API\n";
                 exchange.sendResponseHeaders(200, responseText.getBytes().length);
@@ -20,9 +20,8 @@ public class API {
                 exchange.sendResponseHeaders(405, -1); // 405 Method Not Allowed
             }
             exchange.close();
-        });
-
-
+        }));
+        
         server.setExecutor(null); // creates a default executor
         server.start();
     }
---
+++ demo-Spring-Boot-Rest-API/src/main/java/com/demo/Spring/Boot/Rest/API/DemoSpringBootRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.demo.Spring.Boot.Rest.API;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequestMapping
+@RestController
+
+public class APIController{
+	@GetMapping
+}
