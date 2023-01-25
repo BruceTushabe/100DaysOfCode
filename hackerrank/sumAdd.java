0909d1680c72e94f3f72acf5abac73e6fcc6c4d0
brucetushabe <brucetushabe@gmail.com>
2023-01-25 23:01:38 +0300

    the first file is addition

---
+++ hackerrank/sumAdd.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class sumAdd {
+ 
+
+    public static void main(String[] args) {
+        
+        int a = 2;
+        int b = 3;
+        
+        System.out.println(Integer.sum(a,b));
+            
+    }
+}
