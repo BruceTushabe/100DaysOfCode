9b590ae411151146a8541ca6471b340354eea559
Bruce Tushabe <brucetushabe@gmail.com>
2023-02-02 00:32:31 +0300

    More resolving in the Car class

---
+++ hackerrank/Car.java
@@ -0,0 +1,26 @@
+public class Car {
+
+    int maxSpeed = 180;
+    int minSpeed = 0;
+    double weight = 4089;
+    boolean isTheCarOn = false;
+    char condition = 'A';
+
+
+    public void printVariables(){
+
+        System.out.println("This car has max speed og: " + maxSpeed); 
+        System.out.println(" weight of " + weight);  
+        System.out.println("Is the Car Running ? " + isTheCarOn);
+        System.out.println(condition);
+    
+    }
+
+    public static void main(String[] args) {
+
+        Car kia = new Car();
+        kia.printVariables();
+    
+    }
+    
+}
