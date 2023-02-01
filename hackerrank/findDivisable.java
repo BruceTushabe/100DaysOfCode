03a234811d678365757e205439af4c821d5d85d6
Bruce Tushabe <brucetushabe@gmail.com>
2023-02-01 11:28:48 +0300

    Code is working

--- hackerrank/findDivisable.java
+++ hackerrank/findDivisable.java
@@ -7,25 +7,25 @@ public class findDivisable {
 
         System.out.println("Enter a number divisible by 2: ");
 
-        Scannner sc = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         int x = input.nextInt();
         
-        if (x % 2 == 0)
-          System.out.println("The integer you entered is divisible by 2");
-        
-
-        else 
-            System.out.println("The integer you entered is not divisible by 2, enter another");
-
-            if (x % 5 == 0)
-                {System.out.println("The integer you entered is divisible by 5");}
-            
-            else {
-                
-                System.out.println("The integer you entered is not divisible by 5");
-            
-            
-        }
-    }
-    
-}
+        if (x % 2 == 0){
+            System.out.println("The integer you entered is divisible by 2");
+          }
+  
+          else{
+              System.out.println("The integer you entered is not divisible by 2, enter another");
+          }
+  
+              if (x % 5 == 0)
+                  {System.out.println("The integer you entered is divisible by 5");}
+              
+              else {
+                  
+                  System.out.println("The integer you entered is not divisible by 5");
+          }
+      }
+      
+  }
+  
