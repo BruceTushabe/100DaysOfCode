41a51531a62e4512ddaa9467c600d47adfcd5a65
Bruce Tushabe <brucetushabe@gmail.com>
2023-02-01 16:27:18 +0300

    No idea what is happening but the code isnt working

---
+++ hackerrank/morePractice.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class morePractice {
+
+    public static void main(String[] args) {
+
+        String greeting = "Welcome to 30 days of code!";
+
+        System.out.println("Hello World");
+        System.out.println(greeting);
+
+        Scanner g = new Scanner(System.in);
+
+        double y = g 
+        
+    }
+    
+}
