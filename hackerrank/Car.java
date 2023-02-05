dc146b7d0d3f3a522f09235d893e06290f4e5557
Bruce Tushabe <brucetushabe@gmail.com>
2023-02-05 09:55:09 +0300

    Done Many changes

--- hackerrank/Car.java
+++ hackerrank/Car.java
@@ -9,19 +9,32 @@ public class Car {
     char condition = 'A';
 
 
+    double maxFuel = 16;
+    double currentFuel = 8;
+    double npg = 26.4;
+
+    int numberOfPeopleInCar = 1;
     public void printVariables(){
 
         System.out.println("This car has max speed og: " + maxSpeed); 
         System.out.println(" weight of " + weight);  
         System.out.println("Is the Car Running ? " + isTheCarOn);
-        System.out.println(condition);
+        System.out.println("What condition is it in " + condition);
     
+    }
+
+    public void upgradeMinSpeed (){
+        minSpeed = maxSpeed;
+        maxSpeed = maxSpeed + 1;
     }
 
     public static void main(String[] args) {
 
-        Car kia = new Car();
-        kia.printVariables();
+        Car familyCar = new Car();
+        System.out.println("This is the family car: ");
+        familyCar.printVariables();
+        familyCar.upgradeMinSpeed();
+        familyCar.printVariables();
     
     }
     
