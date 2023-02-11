73413b0413e58c8dbc283c4e9fb3ec96cec40cb6
Bruce Tushabe <brucetushabe@gmail.com>
2023-02-11 23:57:38 +0300

    There is more to do

---
+++ hackerrank/multDarr.java
@@ -0,0 +1,24 @@
+public class multDarr {
+
+
+    public static void main(String args[]) {
+
+
+        int arr[][]  = new arr[2][3];
+
+        arr[0][0] = 2,3;
+        arr[1][1] = 6,9;
+        arr[2][2] = 8,0;
+        arr[3] = 7;
+
+        System.out.println(arr);
+
+
+        
+    }
+
+
+
+
+    
+}
