a24c9d8cf8257c5ae8162a239d812c47c81d1dd3
Bruce Tushabe <brucetushabe@gmail.com>
2023-07-12 23:35:09 +0300

    made improvements on arrays

--- Data Structures/arrays/addArrays.java
+++ Data Structures/arrays/addArrays.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class addArrays {
 
     // Function to add x in arr
@@ -22,29 +24,19 @@ public class addArrays {
    
    // Driver code
    public static void main(String[] args)
-   {
-   
+   {   
        int n = 10;
-       int i;
-   
        // initial array of size 10
-       int arr[]
-           = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-   
+       int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
        // print the original array
-       System.out.println("Initial Array:\n"
-                          + Arrays.toString(arr));
+       System.out.println("Initial Array:\n" + Arrays.toString(arr));
    
        // element to be added
        int x = 50;
-   
        // call the method to add x in arr
-       arr = addX(n, arr, x);
-   
+       arr = addX(n, arr, x);   
        // print the updated array
-       System.out.println("\nArray with " + x
-                          + " added:\n"
-                          + Arrays.toString(arr));
+       System.out.println("\nArray with " + x + " added:\n" + Arrays.toString(arr));
    }
 }
 
