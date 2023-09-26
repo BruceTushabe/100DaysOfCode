70531c041961f4b1497da1c24e2f2c563025640f
Bruce Tushabe <brucetushabe@gmail.com>
2023-09-26 11:04:27 +0300

    No idea why it is misbehaving but I refuse to give up

--- spamD/spamdetection.py
+++ spamD/spamdetection.py
@@ -1,36 +1,4 @@
-import pandas as pd 
+import pandas as pd
 
 
-
-emails = pd.read_csv('spam.csv')
-
-
-from sklearn.feature_extraction.text import CountVectorizer
-vect = CountVectorizer()
-X = vect.fit_transform(emails['text'])
-
-y = emails['spam']
-
-# Train a model
-
-from sklearn.naive_bayes import MultinomialNB
-
-nb = MultinomialNB()
-nb.fit(X,y)
-
-
-# Make predictions
-
-emails_new = pd.read_csv('spam.csv')
-
-X_new = vect.transform(emails_new['text'])
-
-predictions = nb.predict(X_new)
-
-# Evaluate the model
-
-from sklearn.metrics import accuracy_score, precision_score, recall_score
-print(accuracy_score(emails_new['spam'], predictions ))
-print(precision_score(emails_new['spam'], predictions))
-print(recall_score(emails_new['spam'], predictions))
-
+spam = pd.read_csv('spam.csv')
