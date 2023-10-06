9b7dfdcc028e148399e0a553d320d2de39be2b8a
Bruce Tushabe <brucetushabe@gmail.com>
2023-10-06 10:53:55 +0300

    Fun stuff

--- spamD/spamdetection.py
+++ spamD/spamdetection.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-
 spam = pd.read_csv('spam.csv')
+                                                                                                                                                                                                                                                                                          
